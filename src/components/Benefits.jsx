0ee899f75239a4b5c2ddb0396f84923abcd23174
olamilekan29 <farouqolamilekan2006@gmail.com>
2023-10-25 13:00:33 +0100

    completed

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -13,10 +13,10 @@ const Benefits = () =>{
         <div className="md:flex justify-between grid">
         <div className="grid md:pt-[30px] pt-[15px]">
         <img className="md:hidden block w-[300px] pb-[10px]" src={chef} alt="" />
-        <span className="flex md:w-[500px] md:h-[120px] shadow-xl rounded-2xl items-center justify-between md:mb-[40px] w-[300px] h-[90px] mb-[20px]">
+        <span className="flex md:w-[500px] md:h-[120px] shadow-xl rounded-2xl items-center justify-between md:mb-[40px] w-[300px] h-[90px] mb-[20px] text-center">
                 <img className="md:h-[100px] md:w-[200px] rounded pl-[13px] w-[150px] h-[75px]" src={ketchup}/>
-                <span className="grid"> <h4 className="text-[18px] font-semibold text-left ml-[10px]">Mung bean Salad</h4>
-                    <h5 className="text-[13px] font-thin text-left ml-[10px] py-[10px] flex text-[#F6B100]"><svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M13.4375 8.625L8.85156 13L6.5625 10.8125" stroke="#F7B100" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/><path d="M10 18C14.1421 18 17.5 14.6421 17.5 10.5C17.5 6.35786 14.1421 3 10 3C5.85786 3 2.5 6.35786 2.5 10.5C2.5 14.6421 5.85786 18 10 18Z" stroke="#F7B100" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/> </svg>Reduce Chronic Disease Risk</h5>
+                <span className="grid justify-center"> <h4 className="text-[18px] font-semibold text-left ml-[10px]">Mung bean Salad</h4>
+                    <h5 className="text-[13px] font-thin text-left ml-[10px] md:py-[10px] flex text-[#F6B100] py-[0px]"><svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M13.4375 8.625L8.85156 13L6.5625 10.8125" stroke="#F7B100" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/><path d="M10 18C14.1421 18 17.5 14.6421 17.5 10.5C17.5 6.35786 14.1421 3 10 3C5.85786 3 2.5 6.35786 2.5 10.5C2.5 14.6421 5.85786 18 10 18Z" stroke="#F7B100" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/> </svg>Reduce Chronic Disease Risk</h5>
               </span>
              <img className="md:w-[50px] md:block md:h-[20px] md:pr-[10px] hidden" src={rate} alt="" />
         </span> 
@@ -24,7 +24,7 @@ const Benefits = () =>{
         <span className="flex md:w-[500px] md:h-[120px] shadow-xl rounded-2xl items-center justify-between md:mb-[40px] w-[300px] h-[90px] mb-[20px] ">
                 <img className="md:h-[100px] md:w-[200px] rounded pl-[13px] w-[150px] h-[75px]" src={ketchup}/>
                <span className="grid"> <h4 className="text-[18px] font-semibold text-left ml-[10px]">Pasta Salad</h4>
-                            <h5 className="text-[13px] font-thin text-left ml-[10px] py-[10px] flex text-[#F6B100]"><svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <h5 className="text-[13px] font-thin text-left ml-[10px] md:py-[10px] py-[0px] flex text-[#F6B100]"><svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M13.4375 8.625L8.85156 13L6.5625 10.8125" stroke="#F7B100" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
             <path d="M10 18C14.1421 18 17.5 14.6421 17.5 10.5C17.5 6.35786 14.1421 3 10 3C5.85786 3 2.5 6.35786 2.5 10.5C2.5 14.6421 5.85786 18 10 18Z" stroke="#F7B100" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
             </svg>Protects against cancer</h5>
@@ -35,7 +35,7 @@ const Benefits = () =>{
         <span className="flex md:w-[500px] md:h-[120px] shadow-xl rounded-2xl items-center justify-between md:mb-[40px] w-[300px] h-[90px] mb-[20px] ">
                 <img className="md:h-[100px] md:w-[200px] rounded pl-[13px] w-[150px] h-[75px]" src={ketchup}/>
                <span className="grid"> <h4 className="text-[18px] font-semibold text-left ml-[10px]">Tuna Salad</h4>
-                            <h5 className="text-[13px] font-thin text-left ml-[10px] py-[10px] flex text-[#F6B100]"><svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <h5 className="text-[13px] font-thin text-left ml-[10px] md:py-[10px] py-[0px] flex text-[#F6B100]"><svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M13.4375 8.625L8.85156 13L6.5625 10.8125" stroke="#F7B100" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
             <path d="M10 18C14.1421 18 17.5 14.6421 17.5 10.5C17.5 6.35786 14.1421 3 10 3C5.85786 3 2.5 6.35786 2.5 10.5C2.5 14.6421 5.85786 18 10 18Z" stroke="#F7B100" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
             </svg>Helps with Blood Pressure</h5>
@@ -47,7 +47,7 @@ const Benefits = () =>{
         <span className="flex md:w-[500px] md:h-[120px] shadow-xl rounded-2xl items-center justify-between md:mb-[40px] w-[300px] h-[90px] mb-[20px] ">
                 <img className="md:h-[100px] md:w-[200px] rounded pl-[13px] w-[150px] h-[75px]" src={ketchup}/>
                <span className="grid"> <h4 className="text-[18px] font-semibold text-left ml-[10px]">Mung bean Salad</h4>
-                            <h5 className="text-[13px] font-thin text-left ml-[10px] py-[10px] flex text-[#F6B100]"><svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <h5 className="text-[13px] font-thin text-left ml-[10px] md:py-[10px] py-[0px] flex text-[#F6B100]"><svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M13.4375 8.625L8.85156 13L6.5625 10.8125" stroke="#F7B100" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
             <path d="M10 18C14.1421 18 17.5 14.6421 17.5 10.5C17.5 6.35786 14.1421 3 10 3C5.85786 3 2.5 6.35786 2.5 10.5C2.5 14.6421 5.85786 18 10 18Z" stroke="#F7B100" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
             </svg>Reduce Chronic Disease Risk</h5>
@@ -57,8 +57,8 @@ const Benefits = () =>{
 
         <span className="flex md:w-[500px] md:h-[120px] shadow-xl rounded-2xl items-center justify-between md:mb-[40px] w-[300px] h-[90px] mb-[20px] ">
                 <img className="md:h-[100px] md:w-[200px] rounded pl-[13px] w-[150px] h-[75px]" src={ketchup}/>
-               <span className="grid"> <h4 className="text-[18px] font-semibold text-left ml-[10px]">Caser Salad</h4>
-                            <h5 className="text-[13px] font-thin text-left ml-[10px] py-[10px] flex text-[#F6B100]"><svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+               <span className="grid"> <h4 className="md:text-[18px] font-semibold text-left ml-[10px] ">Caser Salad</h4>
+                            <h5 className="text-[13px] font-thin text-left ml-[10px] md:py-[10px] py-[0px] flex text-[#F6B100] text:[10px]"><svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M13.4375 8.625L8.85156 13L6.5625 10.8125" stroke="#F7B100" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
             <path d="M10 18C14.1421 18 17.5 14.6421 17.5 10.5C17.5 6.35786 14.1421 3 10 3C5.85786 3 2.5 6.35786 2.5 10.5C2.5 14.6421 5.85786 18 10 18Z" stroke="#F7B100" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
             </svg>Boosts Immunity System</h5>
